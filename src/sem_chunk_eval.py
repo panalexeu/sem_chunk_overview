51d392b3f92129b490fd1b8236a66ac37087e9e2
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-01-06 12:39:39 +0200

    added TokenWindowSemChunker, reworked sem_chunk_eval.py script to accept chunker name args

--- src/sem_chunk_eval.py
+++ src/sem_chunk_eval.py
@@ -1,24 +1,30 @@
 import time
+import sys
 
 from chunking_evaluation import DatasetEvaluation, Dataset
 from chromadb.utils import embedding_functions
 from rich import print
 
-from window_sem_chunker import WindowSemChunker
+from window_sem_chunker import WindowSemChunker, TokenWindowSemChunker
 
-chunker = WindowSemChunker()
 evaluation = DatasetEvaluation(
     datasets=[
-        Dataset.CHATLOGS,
-        Dataset.PUBMED
+        Dataset.STATE_OF_THE_UNION
     ]
 )
-
 ef = embedding_functions.SentenceTransformerEmbeddingFunction(
     model_name="all-MiniLM-L6-v2"
 )
 
 if __name__ == '__main__':
+    match sys.argv[1]:
+        case 'token':
+            chunker = TokenWindowSemChunker()
+        case 'win':
+            chunker = WindowSemChunker()
+        case _:
+            chunker = WindowSemChunker()
+
     start = time.time()
     results = evaluation.run(chunker, ef)
     end = time.time()
--- src/window_sem_chunker.py
+++ src/window_sem_chunker.py
@@ -1,19 +1,31 @@
 import re
 from typing import List
+from overrides import override
 
+import tiktoken
 from chunking_evaluation import BaseChunker
 from sentence_transformers import SentenceTransformer
+from rich import print
 
 
 class WindowSemChunker(BaseChunker):
-    thresh = 0.9
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+    def __init__(
+            self,
+            thresh=0.9,
+            model: SentenceTransformer = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    ):
+        self.thresh = thresh
+        self.model = model
+
+    def _create_chunks(self, text: str) -> list[str]:
+        return re.split(r'(?<=[.!?])\s+', text)
 
     def split_text(self, text: str) -> List[str]:
-        split_text = re.split(r'(?<=[.!?])\s+', text)
+        split_text = self._create_chunks(text)
 
         prev = ''
-        init = text[0]
+        init = split_text[0]
         chunks = []
 
         for sentence in split_text:
@@ -24,7 +36,9 @@ class WindowSemChunker(BaseChunker):
             )
 
             if dist < self.thresh:
-                print(prev)
+                print('formed chunk: ', prev)
+                print('brk: ', sentence)
+                print('dist: ', dist)
                 print('=' * 50)
 
                 chunks.append(prev)
@@ -37,3 +51,31 @@ class WindowSemChunker(BaseChunker):
             chunks.append(prev)
 
         return chunks
+
+
+class TokenWindowSemChunker(WindowSemChunker):
+
+    def __init__(
+            self,
+            chunk_size: int = 16,
+            thresh=0.9,
+            model: SentenceTransformer = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    ):
+        super().__init__(thresh, model)
+        self.chunk_size = chunk_size
+
+    @override
+    def _create_chunks(self, text: str) -> list[str]:
+        enc = tiktoken.get_encoding('cl100k_base')
+        tokens = enc.encode(text)
+        chunks = []
+        for i in range(0, len(tokens), self.chunk_size):
+            chunk = tokens[i:i + self.chunk_size]
+            print(chunk)
+            print('-' * 50)
+            chunks.append(enc.decode(chunk))
+
+        return chunks
+
+    def split_text(self, text: str) -> List[str]:
+        return super().split_text(text)
